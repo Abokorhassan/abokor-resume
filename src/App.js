e5c42c16a3cd579c3964fa3c70fa7e770b77daf5
abokorhassan <abokor13@hotmail.com>
2019-12-24 23:15:04 +0300

    Made page-container so all page layout align to it

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
     <div className="App">
-      <div className="page"></div>
+      <div className="page">
+        <div className="page-container"></div>
+      </div>
     </div>
   );
 }
