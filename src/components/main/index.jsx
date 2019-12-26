14b289277eb5293e7b611eaed94209b2ff74091d
abokorhassan <abokor13@hotmail.com>
2019-12-26 17:19:43 +0300

    Added Personal Statement to the Main Section

--- src/components/main/index.jsx
+++ src/components/main/index.jsx
@@ -1,8 +1,19 @@
 import React from "react";
 import "./index.css";
+import { Row, Col } from "react-bootstrap";
+import PersonalStatement from "./perseonal-statement";
 
 const Main = () => {
-  return <div className="main"></div>;
+  return (
+    <div className="main">
+      <Row>
+        <Col xs={7}>
+          <PersonalStatement />
+        </Col>
+        <Col xs={5}></Col>
+      </Row>
+    </div>
+  );
 };
 
 export default Main;
---
+++ src/components/main/perseonal-statement.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const PersonalStatement = () => {
+  return (
+    <div id="personal-statement">
+      <h3>PERSONAL STATEMENT</h3>
+      <hr className="hr-headlines" />
+      <div>
+        <p>
+          Energetic Software Engineer with problem-solving skills and strong in
+          math, logic and cross-platform coding. Has developed a whole system
+          for Bus Station Management with a tracking bus location feature.
+          Possess more than 1 year of hands-on experience building web and
+          mobile applications.
+        </p>
+        <p>
+          Now seeking the best opportunities to further enhance the present
+          professional experience and expand the knowledge base on the tech
+          world, while growing with the Organization.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default PersonalStatement;
